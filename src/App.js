e4e695fec4e374210e7b447942123762f1ba8047
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-01-28 21:29:59 +0530

    initial layout

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <p>
-         Initial
-        </p>
+
+     <h1>Is Your Birthday Lucky??</h1>
+
+     <label for="dob-input" className="dob-label"> Date Of Birth</label>
+
+    <input type="date" id="dob-input"></input>
+
+     <label for="num-input" className="num-label">Lucky Number</label>
+
+     <input type="number" id="num-input" placeholder="  Enter a Number"></input>
+
+     <btn id="checkBtn">Check</btn>
+
+     <p id="displayTxt"></p>
+    
         
       </header>
     </div>
